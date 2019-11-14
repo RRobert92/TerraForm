82293f9616091a3683b5f8f8168c4fce0878f82b
fvreddavide <57439205+fvreddavide@users.noreply.github.com>
2019-11-14 19:13:14 +0100

    Update TerraForm_old.ino

--- TerraForm_old.ino
+++ TerraForm_old.ino
@@ -5,16 +5,25 @@
 DHT dht1(DHTPIN1, DHTTYPE);
 DHT dht2(DHTPIN2, DHTTYPE);
 #include <DS3231.h>
-int Relay1 = 8;
-int Relay2 = 7;
-int Relay3 = 4;
-int Relay4 = 12;
+  // defined which Relay is connected to which port
+  int Relay_Rain = 8;
+  int Relay_Light = 7;
+  int Relay_Heat = 4;
+  int Relay_HotSpot = 12;
 DS3231  rtc(SDA, SCL);
 Time t;
 
-const int OnHour[] = {8, 9, 10, 11, 12, 13, 14, 15, 16, 17}; // whatever will be on when the time will be bettwen 8 and 17, number will increase i = i + 1
-const int OffHour[] = {0, 1, 2, 3, 4, 5, 6, 7, 18, 19, 20, 21, 22, 23, 24}; // whatever will be off when the t.hour will be in specifid range of values
+ // defined array for t.hour
+  const int OnHour[] = {8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+  const int OffHour[] = {0, 1, 2, 3, 4, 5, 6, 7, 18, 19, 20, 21, 22, 23, 24};
 
+    //
+  float h1 = dht1.readHumidity();
+  float t1 = dht1.readTemperature();
+  float h2 = dht2.readHumidity();
+  float t2 = dht2.readTemperature();
+  int i;
+  
 #include <Wire.h>
 #include <Adafruit_RGBLCDShield.h>
 #include <utility/Adafruit_MCP23017.h>
@@ -30,146 +39,126 @@ Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 void setup() {
   Serial.begin(115200);
   rtc.begin();
-  pinMode(Relay1, OUTPUT); digitalWrite(Relay1, HIGH);
-  pinMode(Relay2, OUTPUT); digitalWrite(Relay2, HIGH);
-  pinMode(Relay3, OUTPUT); digitalWrite(Relay3, HIGH);
-  pinMode(Relay4, OUTPUT); digitalWrite(Relay4, HIGH);
+  pinMode(Relay_Rain, OUTPUT); digitalWrite(Relay_Rain, HIGH);
+  pinMode(Relay_Light, OUTPUT); digitalWrite(Relay_Light, HIGH);
+  pinMode(Relay_Heat, OUTPUT); digitalWrite(Relay_Heat, HIGH);
+  pinMode(Relay_HotSpot, OUTPUT); digitalWrite(Relay_HotSpot, HIGH);
   dht1.begin(); dht2.begin();
   lcd.begin(16, 2);
-  lcd.print("TerraForm");
+  lcd.print("TerraForm V2");
   lcd.setBacklight(GREEN);
 }
-
-void loop() {
-  int i;
-  t = rtc.getTime();
-  
-  // Serial.print(OffHour[t.hour]);
-  // Serial.print(":"); Serial.print(t.min); Serial.print(":"); Serial.print(t.sec); Serial.println(" ");
-  // delay (1000);
-  lcd.clear(); lcd.setCursor(0, 0);
-  lcd.print(t.hour); lcd.print(":"); lcd.print(t.min); lcd.print(":"); lcd.print(t.sec);
-  delay (1000);
-
-  float h1 = dht1.readHumidity();
-  float t1 = dht1.readTemperature();
+void SaftyHotSpotOff_DHT1(){
   if (isnan(h1) || isnan(t1)) {
   // Serial.println("Failed to read from DHT1 sensor!");
   lcd.print("fail DHT1");
-  digitalWrite(Relay4, HIGH); //turn HOT SPOT OFF
+  digitalWrite(Relay_HotSpot, HIGH);
+}
+}
+
+void SaftyHeatAndRainOff_DHT2(){
+  if (isnan(h2) || isnan(t2)) {
+  // Serial.println("Failed to read from DHT2 sensor!");
+  digitalWrite(Relay_Heat, HIGH);
+  digitalWrite(Relay_Rain, HIGH);
+}
+}
+void TurnOnOffHotSpot(){
+  for(i=0; i<25; i=i+1){
+    if (t1 > 32 || OffHour[i] == t.hour) {
+      digitalWrite(Relay_HotSpot, HIGH);
+      i = 25;
+    }
+    else if (t1 <= 32 && OnHour[i] == t.hour) {
+      digitalWrite(Relay_HotSpot, LOW);
+      i = 25;
+    }
   }
+}
 
-  // Serial.print("Humidity1:"); Serial.print(h1); Serial.print("%");
-  // Serial.print("Temperature1:"); Serial.print(t1); Serial.println("*C ");
-  // delay(1000);
+void TurnOnOffHeat(){
+  if (t2 >= 26) {
+    digitalWrite(Relay_Heat, HIGH);
+  }
+  else if (t2 <= 25) {
+    digitalWrite(Relay_Heat, LOW);
+  }
+}
 
+void TurnOnOffRain(){
+  if (h2 >= 77) {
+    digitalWrite(Relay_Rain, HIGH);
+  }
+  else if (h2 < 77) {
+    digitalWrite(Relay_Rain, LOW);
+  }
+}
+
+void TurnOnOffLight(){
+  for(i=0; i<25; i=i+1){
+    if (OnHour[i] == t.hour) {
+      digitalWrite(Relay_Light, LOW);
+      i = 25;
+    }
+    else if (OffHour[i] == t.hour) {
+      digitalWrite(Relay_Light, HIGH);
+      i = 25;
+    }
+  }
+}
+void ButtonControl(){
+  lcd.setCursor(0, 1);
+  uint8_t ReadButtons = lcd.readButtons();
+
+  if (ReadButtons) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    
+    if (ReadButtons & BUTTON_UP) {
+      digitalWrite(Relay_Light, LOW);
+    }
+    
+    if (ReadButtons & BUTTON_DOWN) {
+      digitalWrite(Relay_Light, HIGH);
+    }
+    if (ReadButtons & BUTTON_LEFT) {
+      lcd.print("LEFT ");
+    }
+    
+    if (ReadButtons & BUTTON_RIGHT) {
+      lcd.print("RIGHT ");
+    }
+    
+    if (ReadButtons & BUTTON_SELECT) {
+      lcd.print("SELECT ");
+    }
+  }}
+  
+void loop(){
+  // display time and do nothing for 1000
+  t = rtc.getTime();
+  lcd.clear(); lcd.setCursor(0, 0);
+  lcd.print(t.hour); lcd.print(":"); lcd.print(t.min); lcd.print(":"); lcd.print(t.sec);
+  delay (1000);
+  
+  // display humidity1 and temp1, do nothing for 1000
   lcd.clear(); lcd.setCursor(0, 0);
   lcd.print("Humi1:"); lcd.print(h1); lcd.print("%");
   lcd.setCursor(0, 1);
   lcd.print("Temp1:"); lcd.print(t1); lcd.print("*C");
   delay(1000);
 
-  float h2 = dht2.readHumidity();
-  float t2 = dht2.readTemperature();
-  if (isnan(h2) || isnan(t2)) {
-  // Serial.println("Failed to read from DHT2 sensor!");
-  // lcd.print("fail DHT2");
-  digitalWrite(Relay3, HIGH); // turn HEAT OFF
-  digitalWrite(Relay1, HIGH); // turn RAIN OFF
-    
-  }
-
-  // Serial.print("Humidity2:"); Serial.print(h2); Serial.print("%");
-  // Serial.print("Temperature2:"); Serial.print(t2); Serial.println("*C ");
-  // delay(1000);
-
+  // display humidity2 and temp2, do nothing for 1000
   lcd.clear(); lcd.setCursor(0, 0);
   lcd.print("Humi2:"); lcd.print(h2); lcd.print("%");
   lcd.setCursor(0, 1);
   lcd.print("Temp2:"); lcd.print(t2); lcd.print("*C");
   delay(1000);
-
-  for(i=0; i<25; i=i+1){
-  if (t1 > 32 || OffHour[i] == t.hour) {
-    digitalWrite(Relay4, HIGH);
-    i = 25;
-    //Serial.println("HOT SPOT OFF");
-    //lcd.clear(); lcd.setCursor(0, 0);
-    //lcd.print("HOT SPOT OFF");
-    //delay (1000);
-  }
-  else if (t1 <= 32 && OnHour[i] == t.hour) {
-    digitalWrite(Relay4, LOW);
-    i = 25;
-    // Serial.println("HOT SPOT ON");
-    // lcd.clear(); lcd.setCursor(0, 0);
-    // lcd.print("HOT SPOT ON");
-    // delay (1000);
-  }
-  }
-
-  if (t2 >= 26) {
-    digitalWrite(Relay3, HIGH);
-    // Serial.println("HEAT OFF");
-    // lcd.clear(); lcd.setCursor(0, 0);
-    // lcd.print("HEAT OFF");
-    // delay (1000);
-  }
-  else if (t2 <= 25) {
-    digitalWrite(Relay3, LOW);
-   // Serial.println("HEAT ON");
-   // lcd.clear(); lcd.setCursor(0, 0);
-   // lcd.print("HEAT ON");
-   // delay (1000);
-  }
   
-  if (h2 >= 77) {
-    digitalWrite(Relay1, HIGH);
-    // Serial.println("RAIN OFF");
-    // lcd.clear(); lcd.setCursor(0, 0);
-    // lcd.print("RAIN OFF");
-    // delay (1000);
-  }
-  else if (h2 < 77) {
-    digitalWrite(Relay1, LOW);
-    // Serial.println("RAIN ON");
-    // lcd.clear(); lcd.setCursor(0, 0);
-    // lcd.print("RAIN ON");
-    // delay (1000);
-  }
-  for(i=0; i<25; i=i+1){
-    if (OnHour[i] == t.hour) {
-    digitalWrite(Relay2, LOW);
-    i = 25;
-    }
-  else if (OffHour[i] == t.hour) {
-    digitalWrite(Relay2, HIGH);
-    i = 25;
-    }
-}
-  lcd.setCursor(0, 1);
-  uint8_t buttons = lcd.readButtons();
-
-  if (buttons) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    if (buttons & BUTTON_UP) {
-    digitalWrite(Relay2, LOW);
-    }
-    if (buttons & BUTTON_DOWN) {
-    digitalWrite(Relay2, HIGH);
-    }
-    if (buttons & BUTTON_LEFT) {
-      lcd.print("LEFT ");
-    
-    }
-    if (buttons & BUTTON_RIGHT) {
-      lcd.print("RIGHT ");
-
-    }
-    if (buttons & BUTTON_SELECT) {
-      lcd.print("SELECT ");
-
-    }
-  }
+  SaftyHotSpotOff_DHT1();
+  SaftyHeatAndRainOff_DHT2();
+  TurnOnOffHotSpot();
+  TurnOnOffHeat();
+  TurnOnOffRain();
+  ButtonControl();
 }
